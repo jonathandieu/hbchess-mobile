1801a92e38d4e748d443c93d4ff1df849dd6c581
Jara-XPS <araj_mont@yahoo.com>
2022-03-20 01:51:03 -0400

    Created login and register page

---
+++ lib/login.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Text('This is the login page'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/register.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Text('This is the register page'),
+        ),
+      ),
+    );
+  }
+}
