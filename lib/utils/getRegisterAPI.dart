229a42106b2765619d0a7903e248f18da4ceb049
rg11145 <rg11145@knights.ucf.edu>
2022-03-27 11:24:50 -0400

    Added register api file, still need to add email verification. Also updated register page.

---
+++ lib/utils/getRegisterAPI.dart
@@ -0,0 +1,37 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const url = 'https://hbchess.app/api/users';
+
+Future<String> doRegister(
+    String username, String email, String password) async {
+  String ret = "";
+  try {
+    http.Response response = await http.post(Uri.parse(url + '/register'),
+        body: jsonEncode({
+          "username": username,
+          "email": email,
+          "password": password,
+        }),
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+        },
+        encoding: Encoding.getByName("utf-8"));
+
+    if (response.statusCode == 201) {
+      ret = "Account Successfully Created";
+    } else if (response.statusCode == 400) {
+      ret = "Invalid user data";
+    } else if (response.statusCode == 404) {
+      ret = "URL Not Found";
+    } else if (response.statusCode == 409) {
+      ret = "Username or email already exists";
+    } else {
+      ret = "Invalid Credentials";
+    }
+  } catch (e) {
+    print(e.toString());
+  }
+  return ret;
+}
