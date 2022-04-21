98c56374f98cba8537fdf320d91680d6f4966920
Jara <araj_mont@yahoo.com>
2022-04-20 20:59:21 -0400

    Leaderboard now functional

--- lib/screens/leaderboard.dart
+++ lib/screens/leaderboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hb_chess/main.dart';
 
 // Dummy data to test leaderboard page
 List<List> teams = [
@@ -32,16 +33,47 @@ class _LeaderboardState extends State<Leaderboard> {
         centerTitle: true,
         title: const Text("Leaderboard"),
       ),
+
+      // Shared Drawer Menu
+      drawer: Drawer(
+        child: SizedBox(
+          height: 2000,
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 31, 41, 55),
+                ),
+                child: Text(
+                  'HBChess',
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Colors.white,
+                    fontFamily: 'Poppins-Regular',
+                  ),
+                ),
+              ),
+              createDrawerTile(Icons.home, 'Dashboard', '/dashboard'),
+              createDrawerTile(
+                  Icons.leaderboard, 'Leaderboard', '/leaderboard'),
+              createDrawerTile(Icons.book, 'Rules', '/rules'),
+              createDrawerTile(Icons.person, 'Teams', '/teams'),
+              createDrawerTile(Icons.settings, 'Settings', '/settings'),
+            ],
+          ),
+        ),
+      ),
       body: Column(
         children: [
           // Leaderboard header
           ConstrainedBox(
-            constraints: BoxConstraints(
+            constraints: const BoxConstraints(
               minHeight: 100,
               maxWidth: 500,
             ),
             child: Container(
-              color: Color.fromARGB(255, 211, 211, 211),
+              color: const Color.fromARGB(255, 211, 211, 211),
               child: Row(
                 children: const <Widget>[
                   Expanded(
@@ -51,7 +83,7 @@ class _LeaderboardState extends State<Leaderboard> {
                     child: Text('Team', textAlign: TextAlign.center),
                   ),
                   Expanded(
-                    child: Text('Rank', textAlign: TextAlign.center),
+                    child: Text('Score', textAlign: TextAlign.center),
                   ),
                 ],
               ),
@@ -67,6 +99,24 @@ class _LeaderboardState extends State<Leaderboard> {
                     (BuildContext context, int index) {
                       return Card(
                         margin: const EdgeInsets.all(5),
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                              color: index == 0
+                                  ? Colors.amber
+                                  : index == 1
+                                      ? Color.fromARGB(255, 173, 173, 173)
+                                      : index == 2
+                                          ? Colors.brown
+                                          : Color.fromARGB(255, 31, 41, 55),
+                              width: index == 0
+                                  ? 5
+                                  : index == 1
+                                      ? 4
+                                      : index == 2
+                                          ? 3
+                                          : 1),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
                         child: Container(
                           height: 80,
                           alignment: Alignment.center,
@@ -75,26 +125,30 @@ class _LeaderboardState extends State<Leaderboard> {
                               // #
                               Expanded(
                                 child: Text('${index + 1}',
-                                    textAlign: TextAlign.center),
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(fontSize: 15),
+                                    ),
                               ),
                               // Team
                               Expanded(
-                                child: Text('${teams[index][0]}',
-                                    textAlign: TextAlign.center),
+                                child: Text(ranks[index].getSenderUser() + ' - ' + ranks[index].getRecipientUser(),
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(fontSize: 15),
+                                    ),
                               ),
                               // Rank
                               Expanded(
-                                child: Text('${teams[index][1]}',
-                                    textAlign: TextAlign.center),
+                                child: Text('${ranks[index].wins.length} - ${ranks[index].draws.length} - ${ranks[index].loss.length}',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(fontSize: 15),
+                                    ),
                               ),
-                              /*
-                        */
                             ],
                           ),
                         ),
                       );
                     },
-                    childCount: teams.length,
+                    childCount: ranks.length,
                   ),
                 ),
               ],
@@ -104,6 +158,16 @@ class _LeaderboardState extends State<Leaderboard> {
       ),
     );
   }
+
+  ListTile createDrawerTile(IconData icon, String title, String route) {
+    //BuildContext context
+    return ListTile(
+        leading: Icon(icon),
+        title: Text(title),
+        onTap: () {
+          Navigator.pushNamed(context, route);
+        });
+  }
 }
 
 // FIXME: Update once rankings are available
@@ -111,6 +175,7 @@ List<List> getPlayers() {
   return teams;
 }
 
+/*
 createPlayerCard(String team, String rank) {
   return Card(
     margin: const EdgeInsets.all(8.0),
@@ -140,3 +205,4 @@ createPlayerCard(String team, String rank) {
     ),
   );
 }
+*/
