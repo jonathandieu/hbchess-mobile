5c99873ca68d9023b4986fd87a84f7f1e33899cc
rg11145 <rg11145@knights.ucf.edu>
2022-04-21 09:18:48 -0400

    updated limit query

--- lib/utils/getLeaderboard.dart
+++ lib/utils/getLeaderboard.dart
@@ -4,45 +4,40 @@ import 'package:hb_chess/utils/getTeamsAPI.dart';
 import 'package:hb_chess/main.dart';
 import 'package:hb_chess/utils/Search.dart';
 
-const url = 'https://hbchess.app/api/teams/all?limit=10&offset=0';
+const url = 'https://hbchess.app/api/teams/all?limit=25&offset=0';
 
-Future<List<Team>> getLeaderboard() async
-{
+Future<List<Team>> getLeaderboard() async {
   List<Team> allTeams = [];
 
-  try
-  {
-    http.Response response = await http.get(Uri.parse(url),
-    headers:
-        {
-          "Accept": "application/json",
-          "Content-Type": "application/json",
-        },
+  try {
+    http.Response response = await http.get(
+      Uri.parse(url),
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
     );
 
     // print(response.statusCode);
     // var responseData = json.decode(response.body);
     // print(responseData[0]["matches"].length);
-    if (response.statusCode == 200)
-    {
+    if (response.statusCode == 200) {
       var data = json.decode(response.body);
-      for (int i = 0; i < data.length; i++)
-      {
-        allTeams.add(
-          Team(
+      for (int i = 0; i < data.length; i++) {
+        allTeams.add(Team(
             id: data[i]['_id'],
-            sender: User(id: data[i]['sender']['_id'], username: data[i]['sender']['username']),
-            recipient: User(id: data[i]['recipient']['_id'], username: data[i]['recipient']['username']),
+            sender: User(
+                id: data[i]['sender']['_id'],
+                username: data[i]['sender']['username']),
+            recipient: User(
+                id: data[i]['recipient']['_id'],
+                username: data[i]['recipient']['username']),
             wins: data[i]['wins'],
             draws: data[i]['ties'],
-            loss: data[i]['losses']
-          )
-        );
+            loss: data[i]['losses']));
       }
     }
-  }
-  catch (e)
-  {
+  } catch (e) {
     print(e.toString());
   }
 
